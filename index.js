91e22231ba19182ac98ded953fd1b82c1d341491
Rahim Spencer <spencer.rahim@gmail.com>
2019-10-17 04:37:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function createRock(x) {
   rock.style.left = `${x}px`
 
   // Hmmm, why would we have used `var` here?
-  var top = 0
+  let top = 0
 
   rock.style.top = top
 
@@ -78,23 +78,28 @@ function createRock(x) {
    * This function moves the rock. (2 pixels at a time
    * seems like a good pace.)
    */
-  function moveRock() {
+  function moveRock(rock) {
     // implement me!
     // (use the comments below to guide you!)
     /**
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
-
+    if (checkCollision(rock)) {
+      endGame();
+    }
     /**
      * Otherwise, if the rock hasn't reached the bottom of
      * the GAME, we want to move it again.
      */
-
+    if (rock.top > 360) {
+      moveRock(rock);
+    } else if (tock.top < 360) {
     /**
      * But if the rock *has* reached the bottom of the GAME,
      * we should remove the rock from the DOM
      */
+     
   }
 
   // We should kick of the animation of the rock around here
